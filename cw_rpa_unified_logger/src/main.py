cacec71d061b6089d13581259fb94654247e9e09
VinnyVanGogh <133192356+VinnyVanGogh@users.noreply.github.com>
2025-01-24 12:38:51 -0800

    add async logger

--- cw_rpa_unified_logger/src/main.py
+++ cw_rpa_unified_logger/src/main.py
@@ -8,7 +8,7 @@ async def cleanup_loggers(logger):
         await discord_logger.cleanup()
     
 async def main():
-    logger, logger_manager = await get_logger("https://discord.com/api/webhooks/1332028030289186908/K1y0oexKLumsr6yGsnph4ww3PzPMoo8abpvyo8NPNziMWzI68P7_lmvbiKer2gkSSc-1")
+    logger, logger_manager = await get_logger("webhook_url")
     if not logger:
         print("Failed to initialize logger")
         return
